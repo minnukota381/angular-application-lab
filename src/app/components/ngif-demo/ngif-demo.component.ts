ff1754a3a5471b7644efa5cab6fe359f85a08252
minnukota381 <minnukota381@gmail.com>
2024-10-09 23:05:40 +0530

    "Added CommonModule import and new properties/method to NgIfDemoComponent"

--- src/app/components/ngif-demo/ngif-demo.component.ts
+++ src/app/components/ngif-demo/ngif-demo.component.ts
@@ -1,16 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-ngif-demo',
   standalone: true,
   templateUrl: './ngif-demo.component.html',
   styleUrls: ['./ngif-demo.component.css'],
+  imports: [CommonModule], // Import CommonModule to use *ngIf
 })
 export class NgIfDemoComponent {
+  // Title for the component
   title: string = 'ngIf Demo';
 
+  // Boolean to control the visibility of the message
   isMessageVisible: boolean = false;
 
+  // Method to toggle message visibility
   toggleMessage() {
     this.isMessageVisible = !this.isMessageVisible;
   }
